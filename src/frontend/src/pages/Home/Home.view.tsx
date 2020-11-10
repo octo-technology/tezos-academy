6cdd489ea9bd2b4ef8b49d89b525b1a86c5a079a
Christophe Charles <ccharles@octo.com>
2020-11-11 00:35:53 +0100

    Add Github link into home page

--- src/frontend/src/pages/Home/Home.view.tsx
+++ src/frontend/src/pages/Home/Home.view.tsx
@@ -207,7 +207,12 @@ export const HomeView = () => {
 
       <HomeFooter>
         <HomeFooterGrid>
-          <img alt="logo" src="/images/logo.svg" />
+          <div>
+            <img alt="logo" src="/images/logo.svg" />
+            <a href="https://github.com/octo-technology/tezos-academy" target="_blank">
+              Github
+            </a>
+          </div>
           <div>
             <p>About Tezos Link</p>
             <a href="mailto:beta@octo.com" target="_blank">
